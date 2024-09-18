e10f4dc1f7a42e0dd71d5d7870dea659453b8185
Ethan Hale <ethanrussellhale@gmail.com>
2024-09-18 14:27:54 -0500

    blue circle

--- sw/src/display.c
+++ sw/src/display.c
@@ -7,7 +7,8 @@ char init_message[] = "hello";
 void display_init(){
     Output_Init();
     //ST7735_InvertColors(False);
-    ST7735_DrawCharS(32, 32, 'p', 0xFFFF, 0, 2);
+//    ST7735_DrawCharS(32, 32, 'p', 0xFFFF, 0, 2);
+		ST7735_DrawCircle(32, 32, ST7735_BLUE);
     
 }
 
