a5b0cb47e467caa931573b16a1a231b155a5506a
maanavkoladia <maanav.koladia@gmail.com>
2024-09-18 13:04:50 -0500

    kicad work

--- sw/src/speaker.c
+++ sw/src/speaker.c
@@ -5,7 +5,7 @@
 //making PB1 digital output pin
 void PortB_Init(void){
     SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;
-    while((SYSCTL_PRGPIO_R&SYSCTL_PRGPIO_R1) != SYSCTL_PRGPIO_R1){};
+    while((SYSCTL_PRGPIO_R&SYSCTL_PRGPIO_R1) == 0){};
 
     GPIO_PORTB_AFSEL_R &= ~0x01;//turn off alternate functionality
     GPIO_PORTB_AMSEL_R &= ~0x01;//turn off analog
